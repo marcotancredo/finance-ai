9779b24d420206760fc1b33b4d3b670dd6293d25
Marco Antonio Tancredo <marcotancredo@gmail.com>
2024-11-09 01:45:02 -0300

    feat: add transactions pie chart component

---
+++ app/(home)/page.tsx
@@ -0,0 +1,49 @@
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import NavBar from "../_components/navbar";
+import SummaryCards from "./_components/summary-cards";
+import TimeSelect from "./_components/time-select";
+import { format, isMatch } from "date-fns";
+import TransactionsPieChart from "./_components/transactions-pie-chart";
+import { getDashboard } from "../_data/get-dashboard";
+
+interface HomeProps {
+  searchParams: { month: string };
+}
+
+const Home = async ({ searchParams: { month } }: HomeProps) => {
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/login");
+  }
+
+  const monthIsInvalid = !month || !isMatch(month, "MM");
+  if (monthIsInvalid) {
+    const currentMonth = format(new Date(), "MM");
+    redirect(`/?month=${currentMonth}`);
+  }
+
+  const dashboard = await getDashboard(month);
+
+  return (
+    <>
+      <NavBar />
+      <div className="space-y-6 p-6">
+        <div className="flex justify-between">
+          <h1 className="text-2xl font-bold">Dashboard</h1>
+          <TimeSelect />
+        </div>
+        <div className="grid-cols-[2fr, 1fr] grid">
+          <div className="flex flex-col gap-6">
+            <SummaryCards month={month} {...dashboard} />
+            <div className="grid-rows grid grid-cols-3 gap-6">
+              <TransactionsPieChart {...dashboard} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
---
+++ app/_data/get-dashboard/index.ts
@@ -0,0 +1,66 @@
+import { db } from "@/app/_lib/prisma";
+import { TransactionType } from "@prisma/client";
+import { TransactionPercentagePerType } from "./types";
+
+export const getDashboard = async (month: string) => {
+  const where = {
+    date: {
+      gte: new Date(`2024-${month}-01`),
+      lt: new Date(`2024-${month}-31`),
+    },
+  };
+  const depositTotal = Number(
+    (
+      await db.transaction.aggregate({
+        where: { ...where, type: "DEPOSIT" },
+        _sum: { amount: true },
+      })
+    )?._sum?.amount,
+  );
+  const investmentsTotal = Number(
+    (
+      await db.transaction.aggregate({
+        where: { ...where, type: "INVESTMENT" },
+        _sum: { amount: true },
+      })
+    )?._sum?.amount,
+  );
+  const expensesTotal = Number(
+    (
+      await db.transaction.aggregate({
+        where: { ...where, type: "EXPENSE" },
+        _sum: { amount: true },
+      })
+    )?._sum?.amount,
+  );
+
+  const balance = depositTotal - investmentsTotal - expensesTotal;
+  const transacationsTotal = Number(
+    (
+      await db.transaction.aggregate({
+        where,
+        _sum: { amount: true },
+      })
+    )?._sum?.amount,
+  );
+
+  const typesPercentage: TransactionPercentagePerType = {
+    [TransactionType.DEPOSIT]: Math.round(
+      (Number(depositTotal || 0) / Number(transacationsTotal)) * 100,
+    ),
+    [TransactionType.EXPENSE]: Math.round(
+      (Number(expensesTotal || 0) / Number(transacationsTotal)) * 100,
+    ),
+    [TransactionType.INVESTMENT]: Math.round(
+      (Number(investmentsTotal || 0) / Number(transacationsTotal)) * 100,
+    ),
+  };
+
+  return {
+    balance,
+    depositTotal,
+    investmentsTotal,
+    expensesTotal,
+    typesPercentage,
+  };
+};
