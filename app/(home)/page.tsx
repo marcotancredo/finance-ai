04e71fab71123f5cdc22c5c82d9eb5b4db9af88f
Marco Antonio Tancredo <marcotancredo@gmail.com>
2024-11-09 10:03:10 -0300

    fix: position of home page elements

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,6 +1,5 @@
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
-import NavBar from "../_components/navbar";
 import SummaryCards from "./_components/summary-cards";
 import TimeSelect from "./_components/time-select";
 import { format, isMatch } from "date-fns";
@@ -8,6 +7,7 @@ import TransactionsPieChart from "./_components/transactions-pie-chart";
 import { getDashboard } from "../_data/get-dashboard";
 import ExpensesPerCategory from "./_components/expenses-per-category";
 import LastTransactions from "./_components/last-transactions";
+import Navbar from "../_components/navbar";
 
 interface HomeProps {
   searchParams: { month: string };
@@ -29,16 +29,16 @@ const Home = async ({ searchParams: { month } }: HomeProps) => {
 
   return (
     <>
-      <NavBar />
-      <div className="flex flex-col space-y-6 overflow-hidden p-6">
+      <Navbar />
+      <div className="flex h-full flex-col space-y-6 overflow-hidden p-6">
         <div className="flex justify-between">
           <h1 className="text-2xl font-bold">Dashboard</h1>
           <TimeSelect />
         </div>
-        <div className="grid-cols-[2fr, 1fr] grid h-full gap-6 overflow-hidden">
+        <div className="grid h-full grid-cols-[2fr,1fr] gap-6 overflow-hidden">
           <div className="flex flex-col gap-6 overflow-hidden">
             <SummaryCards month={month} {...dashboard} />
-            <div className="grid-rows grid grid-cols-3 gap-6">
+            <div className="grid h-full grid-cols-3 grid-rows-1 gap-6 overflow-hidden">
               <TransactionsPieChart {...dashboard} />
               <ExpensesPerCategory
                 expensesPerCategory={dashboard.totalExpensePerCategory}
